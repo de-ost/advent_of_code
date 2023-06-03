f1195cb2b01f2b78b4afb5b53b80084d1fd9e68f
Dennis Ostermann <dennis.ostermann@smail.th-koeln.de>
2023-06-03 21:41:47 +0200

    01

--- 2022/rust_01/src/main.rs
+++ 2022/rust_01/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let mut calories_carrying: Vec<u32> = vec![0];
+    
+    let file = File::open("resources/input.txt").unwrap();
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        match line.as_str() {
+            "" => calories_carrying.push(0),
+
+
+            _ => {
+                let calories = line.parse::<u32>().unwrap();
+                if let Some(last) = calories_carrying.last_mut() {
+                    *last += calories;
+                }
+            }
+        }
+    }
+
+    calories_carrying.sort();
+    calories_carrying.reverse();
+
+    let max_val = calories_carrying.last().unwrap();
+    let sum: u32 = calories_carrying
+        .iter()
+        .take(3)
+        .sum();
+
+    println!("MAX: {}", max_val);
+    println!("SUM OF LAST 3: {}", sum);
 }
