dda30860b00ac93569e3e89969ec269aadd12ec3
Dennis Ostermann <dennis.ostermann@smail.th-koeln.de>
2023-06-05 02:11:58 +0200

    part 2

--- 2022/rust_04/src/main.rs
+++ 2022/rust_04/src/main.rs
@@ -9,28 +9,29 @@ fn main() {
     let file = File::open("resources/input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let mut result = 0;
+    let mut result_contains = 0;
+    let mut result_overlapping = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
 
         let elfs = line.split(",");
-
         let elf_group: Vec<&str> = elfs.collect();
+        let sorted_ranges = sorted_ranges(&elf_group);
 
-        if one_contains_other(elf_group).unwrap() {
-            result += 1;
+        if one_contains_other(&sorted_ranges).unwrap() {
+            result_contains += 1;
+        }
+        if overlapping(&sorted_ranges).unwrap() {
+            result_overlapping += 1;
         }
     }
 
-    println!("Result: {}", result);
+    println!("Result containment: {}", result_contains);
+    println!("Result overlapping: {}", result_overlapping);
 }
 
-fn one_contains_other(elf_group: Vec<&str>) -> Result<bool, &'static str> {
-    if elf_group.len() != 2 {
-        return Err("Too many Elfs");
-    }
-
+fn sorted_ranges(elf_group: &Vec<&str>) -> Vec<(i32, i32)> {
     let mut cleaning_range: Vec<(i32, i32)> = Vec::new();
 
     for elf in elf_group {
@@ -52,8 +53,29 @@ fn one_contains_other(elf_group: Vec<&str>) -> Result<bool, &'static str> {
             a.0.cmp(&b.0)
         }
     });
+    cleaning_range
+}
+
+// Part I
+fn one_contains_other(cleaning_range: &Vec<(i32, i32)>) -> Result<bool, &'static str> {
+    if cleaning_range.len() != 2 {
+        return Err("Too many Elfs");
+    }
 
     if cleaning_range[0].1 >= cleaning_range[1].1 {
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
+
+// Part II
+fn overlapping(cleaning_range: &Vec<(i32, i32)>) -> Result<bool, &'static str> {
+    if cleaning_range.len() != 2 {
+        return Err("Too many Elfs");
+    }
+
+    if cleaning_range[0].1 >= cleaning_range[1].0 {
         Ok(true)
     } else {
         Ok(false)
