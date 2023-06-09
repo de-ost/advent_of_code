8b8bb1f15038f30fe7bd1cfe94ed86bdc9719d56
Dennis Ostermann <dennis.ostermann@smail.th-koeln.de>
2023-06-09 21:37:26 +0200

    part 2 with tests

--- 2022/rust_06/src/main.rs
+++ 2022/rust_06/src/main.rs
@@ -1,44 +1,117 @@
 // https://adventofcode.com/2022/day/6
 
 use std::{
-    fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self},
+    process,
+    str::Chars,
 };
 
 const START_OF_COMMUNICATION_MARKER: usize = 4;
+const START_OF_COMMUNICATION_MESSAGE: usize = 14;
 
 fn main() -> io::Result<()> {
-    let file = File::open("resources/input.txt")?;
-    let reader = BufReader::new(file);
+    let input = include_str!("../resources/input.txt");
 
-    let mut count = 0;
-    let mut buffer_count = 0;
-    let mut buffer: [char; START_OF_COMMUNICATION_MARKER] = [' '; START_OF_COMMUNICATION_MARKER];
-
-    let mut found = false;
-
-    'search: for line in reader.lines() {
-        for character in line?.chars() {
-            if buffer.iter().take(buffer_count).any(|&c| c == character) {
-                buffer_count = 0;
-            }
-
-            if buffer_count == START_OF_COMMUNICATION_MARKER {
-                found = true;
-                break 'search;
-            }
-
-            buffer[buffer_count] = character;
-            buffer_count += 1;
-            count += 1;
-        }
+    if let Some(index) = find_marker(input.chars(), START_OF_COMMUNICATION_MARKER) {
+        println!("Packet found at index {}.", index);
+    } else {
+        eprintln!("Packet not found.");
+        process::exit(1);
     }
 
-    if found {
-        println!("Found at index {}.", count);
+    if let Some(index) = find_marker(input.chars(), START_OF_COMMUNICATION_MESSAGE) {
+        println!("Message found at index {}.", index);
     } else {
-        println!("Not found.");
+        eprintln!("Message not found.");
+        process::exit(2);
     }
 
     Ok(())
+}
+
+fn find_marker(txt: Chars, marker_len: usize) -> Option<i32> {
+    let mut count = 0;
+    let mut buffer: Vec<char> = Vec::new();
+
+    for character in txt {
+        if buffer.len() == marker_len {
+            return Some(count);
+        }
+
+        if let Some(index) = buffer.iter().position(|&e| e == character) {
+            buffer = buffer[index + 1..].to_vec();
+        }
+
+        buffer.push(character);
+        count += 1;
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_1() {
+        let txt = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MARKER).unwrap();
+        assert_eq!(a, 5);
+    }
+
+    #[test]
+    fn test_part_1_2() {
+        let txt = "nppdvjthqldpwncqszvftbrmjlhg";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MARKER).unwrap();
+        assert_eq!(a, 6);
+    }
+
+    #[test]
+    fn test_part_1_3() {
+        let txt = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MARKER).unwrap();
+        assert_eq!(a, 10);
+    }
+
+    #[test]
+    fn test_part_1_4() {
+        let txt = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MARKER).unwrap();
+        assert_eq!(a, 11);
+    }
+
+    #[test]
+    fn test_part_2_1() {
+        let txt = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MESSAGE).unwrap();
+        assert_eq!(a, 19);
+    }
+
+    #[test]
+    fn test_part_2_2() {
+        let txt = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MESSAGE).unwrap();
+        assert_eq!(a, 23);
+    }
+
+    #[test]
+    fn test_part_2_3() {
+        let txt = "nppdvjthqldpwncqszvftbrmjlhg";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MESSAGE).unwrap();
+        assert_eq!(a, 23);
+    }
+
+    #[test]
+    fn test_part_2_4() {
+        let txt = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MESSAGE).unwrap();
+        assert_eq!(a, 29);
+    }
+
+    #[test]
+    fn test_part_2_5() {
+        let txt = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        let a = find_marker(txt.chars(), START_OF_COMMUNICATION_MESSAGE).unwrap();
+        assert_eq!(a, 26);
+    }
 }
