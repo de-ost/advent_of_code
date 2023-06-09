1549cf3b217a078f418bf4a4ca20cb4592b28c57
Dennis Ostermann <dennis.ostermann@smail.th-koeln.de>
2023-06-09 16:05:44 +0200

    finish part 2

--- 2022/rust_05/src/main.rs
+++ 2022/rust_05/src/main.rs
@@ -6,28 +6,35 @@ use std::{
 };
 
 fn main() {
-    let file = File::open("resources/input.txt").unwrap();
+    let file_path = "resources/input.txt";
+
+    // Part 1
+    let file = File::open(file_path).unwrap();
+    let stacks = read_default_stacks(file);
+
+    let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
-    let mut lines = reader.lines();
+    let lines = reader.lines().skip(10);
 
-    let mut stacks: Vec<Vec<char>> = vec![vec![]; 9];
+    print!("Part 1: ");
+    part_1(lines, stacks);
+    println!();
 
-    for line in lines.by_ref().take(8) {
-        let line = line.unwrap();
+    // Part 2
+    let file = File::open(file_path).unwrap();
+    let stacks = read_default_stacks(file);
 
-        for i in 0..9 {
-            let char_idx = 4 * i + 1;
-            let c = line.chars().skip(char_idx).next().unwrap();
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+    let lines = reader.lines().skip(10);
 
-            if c.is_ascii_uppercase() {
-                stacks[i].push(c);
-            }
-        }
-    }
+    print!("Part 2: ");
+    part_2(lines, stacks);
+    println!();
+}
 
-    stacks.iter_mut().for_each(|stack| stack.reverse());
-
-    for line in lines.skip(2) {
+fn part_1(lines: std::iter::Skip<std::io::Lines<BufReader<File>>>, mut stacks: Vec<Vec<char>>) {
+    for line in lines {
         let line = line.unwrap();
         let mut split = line.split_whitespace();
         let split_parser = split.by_ref();
@@ -48,6 +55,60 @@ fn main() {
     for stack in stacks.iter_mut() {
         print!("{}", stack.pop().unwrap());
     }
+}
 
-    println!();
+fn part_2(lines: std::iter::Skip<std::io::Lines<BufReader<File>>>, mut stacks: Vec<Vec<char>>) {
+    for line in lines {
+        let line = line.unwrap();
+        let mut split = line.split_whitespace();
+        let split_parser = split.by_ref();
+
+        let count: usize = split_parser.skip(1).next().unwrap().parse().unwrap();
+        let mut from: usize = split_parser.skip(1).next().unwrap().parse().unwrap();
+        let mut to: usize = split_parser.skip(1).next().unwrap().parse().unwrap();
+
+        from -= 1;
+        to -= 1;
+
+        let mut temp_stack: Vec<char> = Vec::new();
+
+        for _ in 0..count {
+            let value = stacks[from].pop().unwrap();
+            temp_stack.push(value);
+        }
+
+        for _ in 0..count {
+            let value = temp_stack.pop().unwrap();
+            stacks[to].push(value);
+        }
+    }
+
+    for stack in stacks.iter_mut() {
+        print!("{}", stack.pop().unwrap());
+    }
+}
+
+fn read_default_stacks(file: File) -> Vec<Vec<char>> {
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+
+    let mut stacks: Vec<Vec<char>> = vec![vec![]; 9];
+
+    for line in lines.by_ref().take(8) {
+        let line = line.unwrap();
+
+        for i in 0..9 {
+            let char_idx = 4 * i + 1;
+            let c = line.chars().skip(char_idx).next().unwrap();
+
+            if c.is_ascii_uppercase() {
+                stacks[i].push(c);
+            }
+        }
+    }
+
+    stacks
+        .iter_mut()
+        .for_each(|stack: &mut Vec<char>| stack.reverse());
+    stacks
 }
